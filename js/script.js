3c580789b7616ef3d31e9489d9b121597a2ae1f5
ShelaAgaPedrananda <shelaagaps@gmail.com>
2022-10-08 21:29:07 +0700

    Final-Project

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItem = document.querySelectorAll(".navigation a");
+
+const scrollBtn = document.querySelector(".scrollToTop-btn");
